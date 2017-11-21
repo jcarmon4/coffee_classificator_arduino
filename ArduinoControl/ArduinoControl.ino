a0a6b491b7e3efe4292f834183d6ed0134bb067a
Alejocram <alejocram@gmail.com>
2017-11-21 12:01:27 -0500

    Returning the response then ardino had sent the order to move of servo.

--- ArduinoControl/ArduinoControl.ino
+++ ArduinoControl/ArduinoControl.ino
@@ -25,53 +25,53 @@ void take_action(char action) {
     switch (action) {
       case 'l':
         // Turn left.
-        Serial.println("left");
         move_servo(30);
         delay(1000);
         move_servo(90);
+        Serial.println("1");
         break;
       case 'r':
         // Turn right.
-        Serial.println("right");
         move_servo(150);
         delay(1000);
         move_servo(90);
+        Serial.println("1");
         break;
       case 'c':
         // Turn right.
-        Serial.println("right");
+        Serial.println("center");
         move_servo(90);
         break;  
       // attach or detach servo if desired
       case 'd' : 
         while (digitalRead(7)) {} //delay loop until pin 7 is low
         myservo.detach(); //detach servo
-        Serial.println("servo detached");
+        //Serial.println("servo detached");
         goto bailout; //jump over writing to servo
         break;
       case 'a':
         myservo.attach(7); //reattach servo to pin 7
-        Serial.println("servo attached");
+        //Serial.println("servo attached");
         goto bailout;
         break;
     }
 
 bailout: //reenter code loop
-    Serial.print("Last servo command position: ");    
-    Serial.println(myservo.read());
-    Serial.println();
+    //Serial.print("Last servo command position: ");    
+    //Serial.println(myservo.read());
+    //Serial.println();
     read_buffer=""; //empty for next input
 }
 
 void move_servo(int n) {
   // auto select appropriate value
   if(n >= 500){
-    Serial.print("writing Microseconds: ");
-    Serial.println(n);
+    //Serial.print("writing Microseconds: ");
+    //Serial.println(n);
     myservo.writeMicroseconds(n);
   } else {   
-    Serial.print("writing Angle: ");
-    Serial.println(n);
+    //Serial.print("writing Angle: ");
+    //Serial.println(n);
     myservo.write(n); 
   }
 }
